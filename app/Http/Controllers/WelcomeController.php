32dfa98bbcd47f0ab505a2ac87bb271d8db8e18b
= <=>
2025-08-15 01:46:00 +0100

    categories added to category

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class WelcomeController extends Controller
 {
@@ -12,5 +12,10 @@ class WelcomeController extends Controller
 
     public function about(){
         return view('about-us');
+    }
+
+    public function category(){
+        $categories = DB::table('categories')->orderBy('name', 'asc')->get(); // Assuming you have a 'categories' table
+        return view('categories', compact('categories'));
     }
 }
