1b98f094037052d751adac3bae29b7e8329ff69f
= <=>
2025-09-03 11:56:09 +0100

    changes to the academy

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProfileController;
@@ -12,7 +13,11 @@ Route::get('/about-us', [WelcomeController::class, 'about'])->name('about');
 
 Route::get('/category', [CategoryController::class, 'category'])->name('category');
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::prefix('admin')->middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/categories', [AdminCategoryController::class, 'categories'])->name('admin.categories');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
